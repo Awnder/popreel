f23e7c4ac1d82afb96d406100f6a272243550430
Andrew Shiroma <amshiroma@zoho.com>
2025-01-25 16:08:37 -0800

    finished image upload to supabase

--- app/upload/page.js
+++ app/upload/page.js
@@ -1,47 +1,78 @@
 'use client'
 
 import React from 'react'
+import { useSession } from '@clerk/nextjs'
+import { createClient } from '@supabase/supabase-js'
 import { BiSolidCloudUpload } from 'react-icons/bi'
-import PurpleButton from '../components/PurpleButton'
 import { AiOutlineClose, AiOutlinePlus } from 'react-icons/ai'
+import PurpleButton from '../components/PurpleButton'
 
 export default function Upload() {
-  
-  const [fileUrl, setFileUrl] = React.useState(null)
-  const [fileDate, setFileDate] = React.useState(null)
+  // The `useSession()` hook will be used to get the Clerk `session` object
+  const { session } = useSession()
+	
+	// Create a custom supabase client that injects the Clerk Supabase token into the request headers
+	function createClerkSupabaseClient() {
+	  return createClient(
+	    process.env.NEXT_PUBLIC_SUPABASE_URL,
+	    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+	    {
+	      global: {
+	        // Get the custom Supabase token from Clerk
+	        fetch: async (url, options = {}) => {
+		        // The Clerk `session` object has the getToken() method      
+	          const clerkToken = await session?.getToken({
+		          // Pass the name of the JWT template you created in the Clerk Dashboard
+		          // For this tutorial, you named it 'supabase'
+	            template: 'supabase',
+	          })
+	          
+	          // Insert the Clerk Supabase token into the headers
+		        const headers = new Headers(options?.headers)
+	          headers.set('Authorization', `Bearer ${clerkToken}`)
+	          
+	          // Call the default fetch
+	          return fetch(url, {
+	            ...options,
+	            headers,
+	          })
+	        },
+	      },
+	    },
+	  )
+	}
+
+  const [file, setFile] = React.useState(null)
+  const [fileName, setFileName] = React.useState('')
+  const [fileUrl, setFileUrl] = React.useState('')
 
   const onVideoChange = (event) => {
     const files = event.target.files
 
     if (files && files.length > 0) {
-      const file = files[0]
-      console.log(file)
-      setFileUrl(file.name + file + Date.now() )
-      setFileDate(Date.now())
+      setFile(files[0])
+      setFileName(files[0].name)
+      setFileUrl(URL.createObjectURL(files[0]))
     }
   }
 
-  React.useEffect(() => {
-    console.log(fileUrl)
-  }, [fileUrl])
-
   const clearVideo = () => {
-    setFileUrl(null)
-    setFileDate(null)
+    setFile(null)
+    setFileName('')
+    setFileUrl('')
   }
   
-  const uploadVideo = async (file) => {
-    console.log(file)
-    // const cookieStore = await cookies()
-    // const supabase = createClient(cookieStore)
+  const uploadVideo = async () => {
+    const supabase_client = createClerkSupabaseClient()
   
-    // const { data, error } = await supabase.storage.from('videos').upload(`${Date.now()}-${file.name}`, file)
+    const { data, error } = await supabase_client.storage.from('videos').upload(`${fileUrl}-${Date.now()}-${fileName}`, file)
   
-    // if (error) {
-    //   console.error('upload failed:', error.message)
-    // } else {
-    //   console.log('upload successful:', data)
-    // }
+    if (error) {
+      console.error('upload failed:', error.message)
+    } else {
+      console.log('upload successful:', data)
+      clearVideo()
+    }
   }
 
   return (
@@ -59,7 +90,7 @@ export default function Upload() {
                     <span className="font-medium text-white px-2">Clear Video</span>
                   </div>
                 </PurpleButton>
-                <PurpleButton onClick={clearVideo}>
+                <PurpleButton onClick={uploadVideo}>
                   <div className="flex items-center">
                     <AiOutlinePlus color="white" size={20} />
                     <span className="font-medium text-white px-2">Upload Video</span>
