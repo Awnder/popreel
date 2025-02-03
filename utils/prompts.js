248dcb0d20998b71aa2b695991acf6e42edcd576
Andrew Shiroma <amshiroma@zoho.com>
2025-02-02 17:18:48 -0800

    added categories to prompt

--- utils/prompts.js
+++ utils/prompts.js
@@ -1,5 +1,8 @@
 export const prompt = `
         Summarize the main points of this video in two to five sentences.
-        Make sure to mention some of the topics/themes used in the video.
-        Give me the summary ONLY, NOTHING LESS NOTHING MORE. DO NOT say something like 'Here is the summary:'.
+        Include with your summary between one and three categories from the following list: [Sports, Music, Travel, Technology, Food, Fitness, Movies, Books, Fashion, Gaming, Art, Science, History, Photography, Nature].
+        Give me the summary and categories ONLY, NOTHING LESS NOTHING MORE. DO NOT say something like 'Here is the summary:'.
+
+        Example: "Sports. Movies. A soccer player scores a goal in the last minute of the game. The crowd cheers as the winning team runs across the field. The team wins the championship and is given a golden trophy."
+        Example: "Food. A chef prepares a delicious meal of shrimp and pasta. The camera follows the chef as he adds the final touch to the dish with a flourish."
         `;
